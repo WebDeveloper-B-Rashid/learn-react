44c4030871a8b8676e04bb43075083f1e4003207
A F M Bazlur Rashid <brashid1969@gmail.com>
2025-03-19 06:02:06 +0600

    added some content

---
+++ vite-project/src/demo.jsx
@@ -0,0 +1,13 @@
+// @flow strict
+
+import * as React from 'react';
+
+function demo() {
+    return (
+        <div>
+            <h1>I am running</h1>
+        </div>
+    );
+};
+
+export default demo;
